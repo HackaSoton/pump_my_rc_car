b3ca17f514b8830cb9c0efffe79007be74754b2d
ravanis i. (ir1u13) <ir1u13@soton.ac.uk>
2018-05-06 02:39:32 +0100

    rc_car_controller added

---
+++ rc_car_controller.py
@@ -0,0 +1,43 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+	
+import time
+
+
+
+class Lambo:
+
+	def start_engine():
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+
+		GPIO.setup(7,GPIO.OUT)
+		GPIO.setup(11,GPIO.OUT)
+		GPIO.setup(13,GPIO.OUT)
+		GPIO.setup(15,GPIO.OUT)
+	
+
+	def start_forward():
+		GPIO.output(7,GPIO.LOW)
+		GPIO.output(11,GPIO.HIGH)
+
+	def stop_forward():
+		GPIO.output(7,GPIO.HIGH)
+		GPIO.output(11,GPIO.LOW)
+
+	def start_backward():
+		GPIO.output(13,GPIO.LOW)
+		GPIO.output(15,GPIO.HIGH)
+		
+	def stop_backward():	
+		GPIO.output(13,GPIO.HIGH)
+		GPIO.output(15,GPIO.LOW)
+		
+
+lambo = Lambo()
+
+lambo.start_engine()
+lambo.start_forward()
+
